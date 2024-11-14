61a5c9529fa92ac7b0eb83ac463115e95a28900c
daxida <galmaw@hotmail.com>
2024-11-14 09:28:35 +0100

    Export split logic to utils

--- greek_double_accents/double_accents.py
+++ greek_double_accents/double_accents.py
@@ -32,14 +32,13 @@ from greek_double_accents.constants import (
     PRON,
     PRON_GEN,
 )
+from greek_double_accents.utils import split_punctuation, split_text
 
 DEFAULT_PATH = Path(__file__).parent / "etc/book.txt"
 
 WARNINGS = False
 
-PUNCT = re.compile(r"[,.!?;:\n«»\"'·…]")
 VOWEL_ACCENTED = re.compile(r"[έόίύάήώ]")
-LINE_RE = re.compile(r"[^.!?;:…»]+(?:[.!?;:…»\n]+,?)?")
 
 # Import spacy model: greek small.
 model_name = "el_core_news_sm"  # sm / md / lg
@@ -57,34 +56,6 @@ except OSError:
         model_name,
         disable=["parser", "ner", "lemmatizer", "textcat"],
     )
-
-
-def split_punctuation(word: str) -> tuple[str, str | None]:
-    """Splits a word into its base form and any trailing punctuation."""
-    if mtch := PUNCT.search(word):
-        return word[: mtch.start()], word[mtch.start() :]
-    return word, None
-
-
-def split_text(text: str) -> list[list[list[str]]]:
-    return [
-        [line.split(" ") for line in split_paragraph(par)] for par in text.splitlines(keepends=True)
-    ]
-
-
-def join_words(words: list[list[list[str]]]) -> str:
-    """The reverse of split_text."""
-    new_pars = []
-    for par in words:
-        new_line = []
-        for line in par:
-            new_line.append(" ".join(line))
-        new_pars.append("".join(new_line))
-    return "".join(new_pars)
-
-
-def split_paragraph(paragraph: str) -> list[str]:
-    return LINE_RE.findall(paragraph)
 
 
 class State(Enum):
---
+++ greek_double_accents/utils.py
@@ -0,0 +1,32 @@
+import re
+
+PUNCT = re.compile(r"[,.!?;:\n«»\"'·…]")
+LINE_RE = re.compile(r"[^.!?;:…»]+(?:[.!?;:…»\n]+,?)?")
+
+
+def split_punctuation(word: str) -> tuple[str, str | None]:
+    """Splits a word into its base form and any trailing punctuation."""
+    if mtch := PUNCT.search(word):
+        return word[: mtch.start()], word[mtch.start() :]
+    return word, None
+
+
+def split_text(text: str) -> list[list[list[str]]]:
+    return [
+        [line.split(" ") for line in split_paragraph(par)] for par in text.splitlines(keepends=True)
+    ]
+
+
+def join_words(words: list[list[list[str]]]) -> str:
+    """The reverse of split_text."""
+    new_pars = []
+    for par in words:
+        new_line = []
+        for line in par:
+            new_line.append(" ".join(line))
+        new_pars.append("".join(new_line))
+    return "".join(new_pars)
+
+
+def split_paragraph(paragraph: str) -> list[str]:
+    return LINE_RE.findall(paragraph)
